1b242361a1f525aba3fd39e6e548709fb46c4f47
JOÃO BATISTA XAVIER VELOSO JÚNIOR <joao_veloso-junior1@estudante.sesisenai.org.br>
2023-12-01 11:15:20 -0300

    segunda versão - operação de soma

--- opracoes_arit.js
+++ opracoes_arit.js
@@ -1,7 +1,12 @@
 let res = document.getElementById("res")
 
 function somar(){
+    let num1 = Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("num2").value)
 
+    let resultado_soma = num1 + num2
+
+    res.innerHTML = "A soma de " + num1 + "com " + num2 + "é = " + resultado_soma.toFixed(2)
 }
 
 function diminuir(){
